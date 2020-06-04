837f418ade5dacae8e1725d1adc930fe7a37e2de
taku <vidafuku@gmail.com>
2020-06-04 11:25:01 +0800

    first commit

---
+++ run.py
@@ -0,0 +1,33 @@
+# Install required Apps and Modules
+# apt install mpg123
+# pip install gTTS
+# pip install beautifulsoup4
+# pip install markdown
+
+
+from gtts import gTTS
+import os
+import markdown
+from bs4 import BeautifulSoup
+
+
+# text file path
+textfile = "gherkin-review.md" # Select the script file for conversion
+textdir = "scripts_original"
+fullpath = os.path.join(textdir, textfile)
+
+# audio file path
+audiofile = f'{textfile.split(".")[0]}.mp3'
+audiodir = "output_audio"
+audiofull = os.path.join(audiodir, audiofile)
+
+
+# convert markdown to text
+html = markdown.markdown(open(fullpath).read())
+script = "".join(BeautifulSoup(html).findAll(text=True))
+
+
+# initialize tts and save file
+tts = gTTS(script, lang="en-us")
+tts.save(audiofull)
+os.system("mpg123 " + audiofull)
